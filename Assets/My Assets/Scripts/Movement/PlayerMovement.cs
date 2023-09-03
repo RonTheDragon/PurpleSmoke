9bacf41ecaf5211f09e49d73bbb64db9c645947a
RonTheDragon <60644954+RonTheDragon@users.noreply.github.com>
2023-09-03 15:39:28 +0300

    added some stuff

---
+++ Assets/My Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController _characterController => GetComponent<CharacterController>();
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump");
+    }
+}
